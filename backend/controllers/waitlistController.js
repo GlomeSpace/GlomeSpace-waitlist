5f4497cd96e76e5284b2b02f09c8a002b5439488
Atwijukire Ariho Seth <120330466+Ariho-Seth@users.noreply.github.com>
2025-12-02 19:25:16 +0100

    Implemented UX experience on the frontend
    Signed-off-by: Atwijukire Ariho Seth <120330466+Ariho-Seth@users.noreply.github.com>

--- backend/controllers/waitlistController.js
+++ backend/controllers/waitlistController.js
@@ -76,7 +76,7 @@ const addEmail = async (req, res) => {
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>GlomeSpace</title>
+    <title>Confirmation for GlomeSpace Sign up </title>
   </head>
   <body>
     <table
